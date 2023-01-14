53c3a1323268c320e096c360f66e876d3f7fad9f
dan-skw <danielrtfd@gmail.com>
2023-01-15 00:53:44 +0100

    added POST method and decks UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+type DeckProps = {
+  _id: string;
+  title: string;
+}[];
 
 function App() {
   const [title, setTitle] = useState<string>("");
+  const [decks, setDecks] = useState<DeckProps>([]);
 
   const handleCreateClick = (e: React.FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
     fetch("http://localhost:5000/decks", {
       method: "POST",
       body: JSON.stringify({ title }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     }).catch((err) => {
-      console.error("Error: ", err);
+      console.error(`Error: ${err}`);
     });
   };
 
+  useEffect(() => {
+    const fetchDecks = async () => {
+      await fetch("http://localhost:5000/decks")
+        .then(async (res) => {
+          const data = await res.json();
+          console.log(data);
+          setDecks(data);
+        })
+        .catch((err) => {
+          throw new Error(
+            `Network response was not ok. ${err.status} : ${err.statusText}`
+          );
+        });
+    };
+
+    fetchDecks();
+  }, []);
+
   return (
     <div className="App">
-      <main className="h-screen bg-neutral-900 flex flex-col justify-center items-center ">
+      <main className="h-screen bg-neutral-900 flex flex-col gap-8 justify-center items-center ">
+        <ul className="grid grid-cols-3 gap-8 w-1/2 justify-items-center">
+          {decks.map((deck) => (
+            <li
+              className="flex justify-center items-center text-3xl text-neutral-100 h-48 p-6 w-full tranistion-all duration-300 hover:transition-all hover:duration-300 bg-neutral-800 border-2 border-neutral-700 hover:border-2 hover:border-neutral-500 rounded-lg shadow-lg hover:shadow-neutral-800 select-none"
+              key={deck._id}
+            >
+              {deck.title}
+            </li>
+          ))}
+        </ul>
         <h1 className="text-3xl text-gray-100">{title}</h1>
-        <form className="flex flex-col items-center p-8">
-          <div className="flex flec-row">
+        <form className="flex flex-col items-center">
+          <div className="flex">
             <label
               htmlFor="deck-title"
               className="text-3xl text-slate-200 mx-2"
