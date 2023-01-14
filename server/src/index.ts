8ae95d7dc89fc63659f4be0aa980df0d16766c07
dan-skw <danielrtfd@gmail.com>
2023-01-14 22:00:46 +0100

    resolved cors denied access

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,25 +7,25 @@ import { config } from "dotenv";
 config();
 
 const PORT = 5000;
-
-console.log(process.env);
-
 const app = express();
 
 app.use(express.json());
 
 app.post("/decks", async (req: Request, res: Response) => {
+  res.set("Access-Control-Allow-Origin", "*");
+
   const newDeck = new Deck({
     title: req.body.title,
   });
-  const createdDeck = await newDeck
-    .save()
-    .catch((err) => console.log(`Oops! ${err}`));
+  const createdDeck = await newDeck.save();
   res.send(createdDeck);
 });
 
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGO_URL!).then(() => {
-  console.log(`Listening on port ${PORT}... \nConnected to MongoDB...`);
-  app.listen(PORT);
-});
+mongoose
+  .connect(process.env.MONGO_URL!)
+  .then(() => {
+    console.log(`Listening on port ${PORT}... \nConnected to MongoDB...`);
+    app.listen(PORT);
+  })
+  .catch((err) => console.log(`Oops, an error occured! Error message: ${err}`));
