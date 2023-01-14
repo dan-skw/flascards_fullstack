3489aef1bdd9de7986085d151022d47eb0be396d
dan-skw <danielrtfd@gmail.com>
2023-01-14 22:01:05 +0100

    added basic ui with tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState<string>("");
+
+  const handleCreateClick = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    fetch("http://localhost:5000/decks", {
+      method: "POST",
+      body: JSON.stringify({ title }),
+    }).catch((err) => {
+      console.error("Error: ", err);
+    });
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="h-screen bg-neutral-900 flex flex-col justify-center items-center ">
+        <h1 className="text-3xl text-gray-100">{title}</h1>
+        <form className="flex flex-col items-center p-8">
+          <div className="flex flec-row">
+            <label
+              htmlFor="deck-title"
+              className="text-3xl text-slate-200 mx-2"
+            >
+              Deck Title
+            </label>
+            <input
+              id="deck-title"
+              type="text"
+              autoComplete="off"
+              value={title}
+              className="border-2 border-transparent rounded-md bg-neutral-600 px-2 focus:outline-none focus:border-neutral-400 text-white text-lg"
+              onChange={(e: React.ChangeEvent) => {
+                setTitle((e.target as HTMLInputElement).value);
+              }}
+            />
+          </div>
+          <button
+            className="w-1/2 bg-neutral-700 text-gray-100 text-2xl rounded-lg border-2 border-transparent p-2 mt-4 hover:border-2 hover:border-gray-300 transition-colors duration-300 hover:transition-colors"
+            onClick={(e: React.FormEvent<HTMLButtonElement>) =>
+              handleCreateClick(e)
+            }
+          >
+            Create Deck
+          </button>
+        </form>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
